94c2935e2b8b507df040d9c6a4c897d9e13dcaeb
seyoung5744 <seyoung574458@gmail.com>
2023-02-05 15:41:36 +0900

    Init commit

---
+++ src/main/java/com/zerobase/withmarket/WithMarketApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.withmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/withmarket/WithMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.withmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
