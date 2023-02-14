c3f603b5c34b03a7d64c84afaba88b30710065db
Kodongwoo <ehddn2202@naver.com>
2023-02-14 17:43:14 +0900

    fix(product): 베스트 상품 redis에 객체로 저장
    redis에서 베스트 상품 id 목록을 가지지 않고
    
    redis에서 바로 베스트 상품 객체목록들을 가지고 조회 가능하도록
    수정하였습니다.

--- src/main/java/com/zerobase/wishmarket/domain/product/service/ProductService.java
+++ src/main/java/com/zerobase/wishmarket/domain/product/service/ProductService.java
@@ -4,19 +4,18 @@ import com.zerobase.wishmarket.domain.product.exception.ProductErrorCode;
 import com.zerobase.wishmarket.domain.product.exception.ProductException;
 import com.zerobase.wishmarket.domain.product.model.entity.Product;
 import com.zerobase.wishmarket.domain.product.model.entity.ProductLikes;
+import com.zerobase.wishmarket.domain.product.model.entity.RedisBestProducts;
 import com.zerobase.wishmarket.domain.product.model.type.ProductCategory;
 import com.zerobase.wishmarket.domain.product.repository.ProductLikesRepository;
 import com.zerobase.wishmarket.domain.product.repository.ProductRepository;
-import java.util.HashSet;
+import com.zerobase.wishmarket.domain.product.repository.RedisBestRepository;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,10 +27,9 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ProductLikesRepository productLikesRepository;
 
-    private final RedisTemplate<String, String> redisTemplate;
-    private static SetOperations<String, String> setOperations;
+    private final RedisBestRepository redisBestRepository;
 
-    private static final String KEY_BEST_PRODUCTS = "BEST_PRODUCTS";
+    private static final Long KEY_BEST_PRODUCTS = 1L;
 
 
     //임시로 제품 정보 넣기, Batch로 서버 시작시 데이터 저장, 추후 변경
@@ -39,7 +37,7 @@ public class ProductService {
         for (ProductCategory category : ProductCategory.values()) {
             for (int i = 1; i < 21; i++) {
                 Product product = Product.builder()
-                    .name("제품" + i)
+                    .name("product" + i)
                     .productImage("제품" + i + "파일 경로")
                     .category(category)
                     .price(1000)
@@ -57,50 +55,33 @@ public class ProductService {
 
         //베스트 상품 등록을 위해 likes 수 몇개만 변경
         for (Long i = 1L; i < 13L; i++) {
-            ProductLikes productLikes = productLikesRepository.findById(i).orElseThrow(()->new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND));
+            ProductLikes productLikes = productLikesRepository.findById(i)
+                .orElseThrow(() -> new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND));
             productLikes.setLikes(10);
         }
     }
 
-    //베스트 상품 12개 업데이트
+
+    //베스트 상품 N개 업데이트
     public boolean updateBestProducts() {
 
-        setOperations = redisTemplate.opsForSet();
+        //베스트 상품
+        redisBestRepository.deleteAll();
 
-        //베스트 상품을 삭제하기 전에, 상품 데이터에 isBest가 true인 데이터들을 false로 초기화
-        List<Product> products = productRepository.findAll();
-        for(Product product : products){
-            if(product.isBest() == true ){
-                product.setIsBestFalse();
-            }
-        }
+        //문제의 정렬 부분
+        List<ProductLikes> bestProductLikes = productLikesRepository.findTop50ByOrderByLikesDesc();
 
-        //redis에 남아있던 베스트 상품 정보 삭제
-        redisTemplate.delete(KEY_BEST_PRODUCTS);
-
-
-        //Redis에 새로 저장할 베스트 상품 id 목록 업데이트
-        List<ProductLikes> bestProductLikes = productLikesRepository.findTop12ByOrderByLikesDesc();
-
+        List<Long> ids = new ArrayList<>();
         for (int i = 0; i < bestProductLikes.size(); i++) {
-            setOperations.add(KEY_BEST_PRODUCTS,
-                Long.toString(bestProductLikes.get(i).getProductId()));
+            ids.add(bestProductLikes.get(i).getProductId());
         }
 
-        Set<String> bestSets = setOperations.members(KEY_BEST_PRODUCTS);
-        Set<Long> bestListToLong = new HashSet<>();
-
-        //Long 타입으로 전환
-        for (String s : bestSets) {
-            bestListToLong.add(Long.valueOf(s));
-        }
-
-        //Redis에 저장된 베스트 상품id에 따라, 상품 데이터의 isBest = true로 변경
-        for (Long productId : bestListToLong) {
-            Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductException(ProductErrorCode.PRODUCT_NOT_FOUND));
-            product.setIsBestTrue();
-        }
+        //redis repository에 넣기
+        List<Product> productList = productRepository.findAllByProductIdIn(ids);
+        redisBestRepository.save(RedisBestProducts.builder()
+            .id(KEY_BEST_PRODUCTS)
+            .products(productList)
+            .build());
 
         return true;
     }
@@ -116,25 +97,10 @@ public class ProductService {
 
     //베스트 상품 조회
     public List<Product> getBestProducts() {
-
-        //Redis에 저장되어있는 Best 상품에 대한 ID들을 가져옴
-        Set<String> bestSets = setOperations.members(KEY_BEST_PRODUCTS);
-        if (bestSets.isEmpty()) {
-            throw new ProductException(ProductErrorCode.BEST_PRODUCT_NOT_FOUND);
-        }
-
-        //Long 타입으로 전환 (redis가 Long타입으로 저장이 안되는데 아시는분은 말씀해주세요!,,)
-        Set<Long> bestListToLong = new HashSet<>();
-        for (String s : bestSets) {
-            bestListToLong.add(Long.valueOf(s));
-        }
-
-        List<Product> productList = productRepository.findAllByProductIdIn(bestListToLong);
-        if (productList.isEmpty()) {
-            throw new ProductException(ProductErrorCode.BEST_PRODUCT_NOT_FOUND);
-        }
-
+        List<Product> productList = redisBestRepository.findById(KEY_BEST_PRODUCTS).get().getProducts();
         return productList;
     }
+
+
 
 }
