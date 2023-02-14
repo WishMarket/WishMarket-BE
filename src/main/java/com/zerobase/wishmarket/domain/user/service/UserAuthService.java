3f2ec1269b6408c4a32aadc546b8623636520ba9
seyoung5744 <seyoung574458@gmail.com>
2023-02-14 19:02:43 +0900

    fix : Acess Token 만료 시간 수정

--- src/main/java/com/zerobase/wishmarket/domain/user/service/UserAuthService.java
+++ src/main/java/com/zerobase/wishmarket/domain/user/service/UserAuthService.java
@@ -1,5 +1,6 @@
 package com.zerobase.wishmarket.domain.user.service;
 
+import static com.zerobase.wishmarket.common.jwt.model.constants.JwtConstants.REFRESH_TOKEN_PREFIX;
 import static com.zerobase.wishmarket.common.jwt.model.constants.JwtConstants.TOKEN_PREFIX;
 import static com.zerobase.wishmarket.domain.user.exception.UserErrorCode.ALREADY_REGISTER_USER;
 import static com.zerobase.wishmarket.domain.user.exception.UserErrorCode.EMAIL_NOT_FOUND;
@@ -9,6 +10,7 @@ import static com.zerobase.wishmarket.domain.user.exception.UserErrorCode.PASSWO
 
 import com.zerobase.wishmarket.common.jwt.JwtAuthenticationProvider;
 import com.zerobase.wishmarket.common.jwt.model.dto.TokenSetDto;
+import com.zerobase.wishmarket.common.redis.RedisClient;
 import com.zerobase.wishmarket.domain.user.exception.UserException;
 import com.zerobase.wishmarket.domain.user.model.dto.SignInForm;
 import com.zerobase.wishmarket.domain.user.model.dto.SignInResponse;
@@ -18,7 +20,9 @@ import com.zerobase.wishmarket.domain.user.model.entity.UserEntity;
 import com.zerobase.wishmarket.domain.user.model.type.UserRegistrationType;
 import com.zerobase.wishmarket.domain.user.model.type.UserStatusType;
 import com.zerobase.wishmarket.domain.user.repository.UserAuthRepository;
+import java.util.Date;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +38,7 @@ public class UserAuthService {
     private final UserAuthRepository userAuthRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtAuthenticationProvider jwtProvider;
+    private final RedisClient redisClient;
 
     @Transactional
     public SignUpEmailResponse signUp(SignUpForm form) {
@@ -83,15 +88,29 @@ public class UserAuthService {
             .orElseThrow(() -> new UserException(EMAIL_NOT_FOUND));
 
         validationPassword(form.getPassword(), user);
+
         TokenSetDto tokenSetDto = jwtProvider.generateTokenSet(user.getUserId());
+
+        // 작성된 날짜에서 현재 날짜를 빼고 밀리초로 나누면 지나간 시간(초)이 계산
+        long expirationSeconds = (tokenSetDto.getRefreshTokenExpiredAt().getTime() - new Date().getTime()) / 1000;
+
+        // redis에 refresh토큰 저장
+        redisClient.put(
+            REFRESH_TOKEN_PREFIX + String.valueOf(user.getUserId()),
+            tokenSetDto.getRefreshToken(),
+            TimeUnit.SECONDS,
+            expirationSeconds);
 
         return SignInResponse.builder()
             .email(user.getEmail())
             .name(user.getName())
             .accessToken(TOKEN_PREFIX + tokenSetDto.getAccessToken())
+            .accessTokenExpiredAt(String.valueOf(jwtProvider.getExpiredDate(tokenSetDto.getAccessToken())))
             .refreshToken(tokenSetDto.getRefreshToken())
+            .refreshTokenExpiredAt(String.valueOf(jwtProvider.getExpiredDate(tokenSetDto.getRefreshToken())))
             .build();
     }
+
 
     private void validationPassword(String password, UserEntity user) {
         if (!this.passwordEncoder.matches(password, user.getPassword())) {
