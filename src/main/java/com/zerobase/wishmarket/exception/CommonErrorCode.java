c3f41096aafc187de586a7fc77afc5ba732e53a2
seyoung5744 <seyoung574458@gmail.com>
2023-02-15 00:21:01 +0900

    feat : S3 파일 업로드 및 삭제
    - S3에 이미지 파일을 업로드 합니다.
    - S3에 directory를 기반으로 파일을 삭제합니다.

--- src/main/java/com/zerobase/wishmarket/exception/CommonErrorCode.java
+++ src/main/java/com/zerobase/wishmarket/exception/CommonErrorCode.java
@@ -11,16 +11,18 @@ public enum CommonErrorCode implements ErrorCode{
     REDIS_PUT_EMPTY_KEY(HttpStatus.BAD_REQUEST, "Empty Key를 입력하였습니다."),
     REDIS_PUT_FAIL(HttpStatus.BAD_REQUEST, "잘못된 Key를 입력하였습니다."),
     DUPLICATION_KEY(HttpStatus.BAD_REQUEST, "중복된 Key입니다."),
-
+    EMPTY_FILE(HttpStatus.BAD_REQUEST, "업로드한 파일이 없습니다."),
+    FILE_SIZE_EXCEED(HttpStatus.BAD_REQUEST, "파일 업로드 용량을 초과하였습니다."),
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
-    EXPIRED_KEY(HttpStatus.NOT_FOUND, "해당 Key는 이미 만료되었습니다.")
+    EXPIRED_KEY(HttpStatus.NOT_FOUND, "해당 Key는 이미 만료되었습니다."),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
 
     /* 500 Internal Server Error : 서버가 처리 방법을 모르는 상황이 발생. 서버는 아직 처리 방법을 알 수 없음.*/
-
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 전송에 실패하였습니다."),
+    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제에 실패하였습니다.")
     ;
 
     private final HttpStatus errorCode;
--- src/main/java/com/zerobase/wishmarket/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/wishmarket/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,6 +15,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleServiceException(GlobalException e) {
         ErrorResponse response = new ErrorResponse(e.getErrorCode().getErrorCode(), e.getMessage());
 
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 파일 업로드 용량 초과시 발생
+     */
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    protected ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(
+        GlobalException e) {
+        log.info("handleMaxUploadSizeExceededException", e);
+
+        ErrorResponse response = new ErrorResponse(e.getErrorCode().getErrorCode(), e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
