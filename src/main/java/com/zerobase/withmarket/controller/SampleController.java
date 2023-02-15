19973fb55a2b8dbb904a4c5f57ee0b82b7f15f42
seyoung5744 <seyoung574458@gmail.com>
2023-02-15 15:28:59 +0900

    fix : 테스트 확인을 위한 sample controller 수정

--- src/main/java/com/zerobase/withmarket/controller/SampleController.java
+++ src/main/java/com/zerobase/withmarket/controller/SampleController.java
@@ -8,6 +8,6 @@ public class SampleController {
 
     @GetMapping("sample")
     public String sample(){
-        return "안녕하세요!!!~~~성공!!!!";
+        return "안녕하세요!!!~~~성공!!!! \n CORS 정책을 위해 WebMvcConfig를 추가했습니다.";
     }
 }
