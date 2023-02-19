06fb6d5aa9ecf7ab06477c9e6e94139622dbd25d
paik-dongjoo <d5ngjoo@naver.com>
2023-02-20 02:31:57 +0900

    fix(User) : 회원정보 조회기능 구현(#64)
    - commit message title 수정

--- src/main/java/com/zerobase/wishmarket/domain/user/repository/UserRepository.java
+++ src/main/java/com/zerobase/wishmarket/domain/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.zerobase.wishmarket.domain.user.repository;
 
-import com.zerobase.wishmarket.domain.user.model.dto.UserDto;
 import com.zerobase.wishmarket.domain.user.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
