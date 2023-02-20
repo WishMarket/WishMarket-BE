9b018722e3f482e199f0c6eda9705fd621eed097
김선범 <sktjsqja@gmail.com>
2023-02-20 21:51:21 +0900

    fix(Point) : URL 수정  1. URL수정

--- src/main/java/com/zerobase/wishmarket/domain/point/controller/PointController.java
+++ src/main/java/com/zerobase/wishmarket/domain/point/controller/PointController.java
@@ -5,16 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/point")
 @RestController
 public class PointController {
     private final PointService pointService;
 
-    @PutMapping("/") //1만포인트를 일괄적으로 증가시키도록 설정함
+    @PutMapping("/api/point/increase") //1만포인트를 일괄적으로 증가시키도록 설정함
     public ResponseEntity<?> addPoint(@AuthenticationPrincipal Long userId) {
 
         return ResponseEntity.ok().body(pointService.chargePoint(userId));
