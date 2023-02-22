f6e566844d2ca07a4dc66294692c63d9c812a488
Kodongwoo <ehddn2202@naver.com>
2023-02-22 15:09:18 +0900

    feat(Funding): 펀딩 API - 기본구성 요소 생성
    펀딩 컨트롤러
    
    펀딩 서비스
    
    펀딩 레퍼지토리
    
    펀딩 예외처리 파일을 생성하였습니다.

---
+++ src/main/java/com/zerobase/wishmarket/domain/funding/controller/FundingController.java
@@ -0,0 +1,9 @@
+package com.zerobase.wishmarket.domain.funding.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+public class FundingController {
+
+}
