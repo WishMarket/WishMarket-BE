2b022c7a63a92fb1877f4d4b401d5cdd3d0a5bce
paik-dongjoo <d5ngjoo@naver.com>
2023-02-23 22:45:54 +0900

    fix(User) : 유저 엔티티 생성 시점 - follow 관련정보 초기화(#66)

--- src/main/java/com/zerobase/wishmarket/domain/user/service/OAuthService.java
+++ src/main/java/com/zerobase/wishmarket/domain/user/service/OAuthService.java
@@ -1,10 +1,11 @@
 package com.zerobase.wishmarket.domain.user.service;
 
+import com.zerobase.wishmarket.domain.follow.model.entity.FollowInfo;
+import com.zerobase.wishmarket.domain.follow.repository.FollowInfoRepository;
 import com.zerobase.wishmarket.domain.user.model.dto.OAuthAttributes;
 import com.zerobase.wishmarket.domain.user.model.dto.OAuthUserInfo;
 import com.zerobase.wishmarket.domain.user.model.entity.UserEntity;
 import com.zerobase.wishmarket.domain.user.repository.UserAuthRepository;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -16,12 +17,15 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserAuthRepository userAuthRepository;
+    private final FollowInfoRepository followInfoRepository;
     private final HttpSession httpSession;
 
     @Override
@@ -33,26 +37,39 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
         // 현재 로그인 진행중인 서비스를 구분 (추후 네이버 등 연동 시 구분 필요)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-            // 키가 되는 필드값 (Primary Key와 같은 의미)
-            .getUserInfoEndpoint().getUserNameAttributeName();
+                // 키가 되는 필드값 (Primary Key와 같은 의미)
+                .getUserInfoEndpoint().getUserNameAttributeName();
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName,
-            oAuth2User.getAttributes());
+                oAuth2User.getAttributes());
 
         UserEntity userEntity = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new OAuthUserInfo(userEntity));
 
         return new DefaultOAuth2User(
-            Collections.singleton(new SimpleGrantedAuthority("USER")),
-            attributes.getAttributes(),
-            attributes.getNameAttributeKey());
+                Collections.singleton(new SimpleGrantedAuthority("USER")),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
     }
 
     private UserEntity saveOrUpdate(OAuthAttributes attributes) {
-        UserEntity userEntity = userAuthRepository.findByEmail(attributes.getEmail())
-            .map(entity -> entity.update(attributes.getName(), attributes.getProfileImage()))
-            .orElse(attributes.toEntity(attributes));
+        Optional<UserEntity> userEntity = userAuthRepository.findByEmail(attributes.getEmail());
 
-        return userAuthRepository.save(userEntity);
+        if (!userEntity.isPresent()) {
+            attributes.toEntity(attributes);
+
+            FollowInfo emptyFollowInfo = FollowInfo.builder()
+                    .followerCount(0L)
+                    .followCount(0L)
+                    .build();
+
+            followInfoRepository.save(emptyFollowInfo);
+        } else {
+            userEntity.map(entity -> entity.update(attributes.getName(), attributes.getProfileImage()));
+        }
+
+        return userEntity.get();
     }
+
+
 }
