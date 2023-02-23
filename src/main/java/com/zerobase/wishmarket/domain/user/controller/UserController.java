df771b848c26c939c8dfaeef2f04904dc66092e8
seyoung5744 <seyoung574458@gmail.com>
2023-02-24 02:59:40 +0900

    fix : 비밀번호 변경 api method post -> patch로 변경

--- src/main/java/com/zerobase/wishmarket/domain/user/controller/UserController.java
+++ src/main/java/com/zerobase/wishmarket/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +32,7 @@ public class UserController {
         return ResponseEntity.ok(userInfo);
     }
 
-    @PostMapping("/password")
+    @PatchMapping("/password")
     public ResponseEntity<UserPasswordChangeReturnType> passwordChange(@RequestBody @Valid ChangePwdForm form) {
         return ResponseEntity.ok(userService.passwordChange(form));
     }
