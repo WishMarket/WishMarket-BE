c1e17bd330983e163a091511843a6ac78038f69d
김선범 <sktjsqja@gmail.com>
2023-02-23 19:33:28 +0900

    fix(Product) : Detail & Review 수정 1. DetailDto - isBest 컬럼 추가 2. Paging - Query parameter 형식통일             (page&size 요청) 3. ReviewDto 생성 4. ReviewDto 생성에 따른 반환타입 수정 5. ReviewTest 작성

--- src/main/java/com/zerobase/wishmarket/domain/product/controller/ProductController.java
+++ src/main/java/com/zerobase/wishmarket/domain/product/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.zerobase.wishmarket.domain.product.controller;
 
-import com.zerobase.wishmarket.domain.product.model.ProductInputForm;
 import com.zerobase.wishmarket.domain.product.model.dto.ProductBestDto;
 import com.zerobase.wishmarket.domain.product.model.dto.ProductCategoryDto;
 import com.zerobase.wishmarket.domain.product.model.dto.ProductDetailDto;
@@ -13,9 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,13 +45,13 @@ public class ProductController {
 
     @GetMapping("/search")
     public Page<ProductSearchDto> search(@RequestParam("keyword") String keyword,
-        @RequestParam("page") Integer page) {
-        PageRequest pageRequest = PageRequest.of(page - 1, 12);
+        @RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
         return ResponseEntity.ok().body(productService.search(keyword, pageRequest)).getBody();
     }
 
     @GetMapping("/{productId}/detail")
-    public ResponseEntity<?> productDetail(@PathVariable Long productId) {
+    public ResponseEntity<ProductDetailDto> productDetail(@PathVariable Long productId) {
         ProductDetailDto responseDto = productService.detail(productId);
         return ResponseEntity.ok().body(responseDto);
     }
