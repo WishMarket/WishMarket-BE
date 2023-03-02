96d5fa5a095bfcc88391dd565758012ce33e8f90
seyoung5744 <seyoung574458@gmail.com>
2023-03-03 00:07:36 +0900

    fix : 프론트 jwt 재발급 테스트를 위한 시간 재조정

--- src/main/java/com/zerobase/wishmarket/common/jwt/model/constants/JwtConstants.java
+++ src/main/java/com/zerobase/wishmarket/common/jwt/model/constants/JwtConstants.java
@@ -6,7 +6,7 @@ public interface JwtConstants {
 
     String TOKEN_HEADER = "Authorization";
     String ACCESS_TOKEN_PREFIX = "Bearer ";
-    long ACCESS_TOKEN_VALID_TIME = Duration.ofSeconds(180).toMillis(); // Access Token: 12시간
+    long ACCESS_TOKEN_VALID_TIME = Duration.ofSeconds(120).toMillis(); // Access Token: 12시간
     long REFRESH_TOKEN_VALID_TIME = Duration.ofSeconds(540).toMillis(); // 만료 기간 30일
     long ACCESS_REFRESH_TOKEN_REISSUE_TIME = Duration.ofSeconds(360).toMillis(); // 15일
     String REFRESH_TOKEN_PREFIX = "Refresh:";
