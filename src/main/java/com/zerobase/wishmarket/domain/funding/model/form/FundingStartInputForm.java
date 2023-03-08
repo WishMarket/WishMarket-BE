acc6224b16c3fcddac952702e2e29c51619fd711
Kodongwoo <ehddn2202@naver.com>
2023-03-08 18:31:53 +0900

    fix(Funding): @JsonFormat형식 변경

--- src/main/java/com/zerobase/wishmarket/domain/funding/model/form/FundingStartInputForm.java
+++ src/main/java/com/zerobase/wishmarket/domain/funding/model/form/FundingStartInputForm.java
@@ -2,8 +2,6 @@ package com.zerobase.wishmarket.domain.funding.model.form;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,20 +18,13 @@ public class FundingStartInputForm {
     private Long targetId;
     private Long fundedPrice;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "GMT+9")
     private LocalDateTime startDate;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+9")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "GMT+9")
     private LocalDateTime endDate;
-
-    public LocalDateTime getStartZoneDate() {
-        return ZonedDateTime.of(startDate, ZoneId.of("Asia/Seoul")).toLocalDateTime();
-    }
-    public LocalDateTime getEndZoneDate() {
-        return ZonedDateTime.of(endDate,ZoneId.of("Asia/Seoul")).toLocalDateTime();
-    }
 
 
 }
